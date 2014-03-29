436796304e0f2c4ae71188c12ec0573155519e53
jgutierrez <jgutierrez@switchfly.com>
2014-03-29 16:16:25 -0600

    include yowsup library models

--- app/Models.py
+++ app/Models.py
@@ -1,6 +1,25 @@
 from google.appengine.ext import ndb
 
-class Account(ndb.Model):
-    username = ndb.StringProperty()
-    userid = ndb.IntegerProperty()
+
+class User(ndb.Model):
+    user_id = ndb.IntegerProperty()
+    pss = ndb.StringProperty()
+    name = ndb.StringProperty()
     email = ndb.StringProperty()
+    tel_country_code = ndb.IntegerProperty()
+    tel = ndb.IntegerProperty()
+    supInfo = WhatsupInfo()
+    faceInfo = FacebookInfo()
+    contacts = Contacts()
+
+
+class WhatsupInfo(ndb.Model):
+    pss = ndb.StringProperty()
+
+
+class FacebookInfo(ndb.Model):
+    pss = ndb.StringProperty()
+
+
+class Contacts(ndb.Model):
+    users = ndb.StructuredProperty(User, repeated=True)
